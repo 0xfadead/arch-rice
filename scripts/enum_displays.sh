82bca17f250785468f3a38bb66b91d88915fc688
kkuehler <keur@ocf.berkeley.edu>
2017-12-21 02:31:01 -0800

    Power duals on startup

---
+++ scripts/enum_displays.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# all displays detected by the OS 
+"$CONNECTED"=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+# start with primary display 
+"$LAST"=$(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+
+for display in $CONNECTED
+do
+    if [[ "$display" == "$LAST" ]]; then
+        continue 
+    fi
+    xrandr --ouput "$display" --auto --right-of "$LAST" 
+done
