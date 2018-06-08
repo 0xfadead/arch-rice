3fb19ddddee2ef79f47d6c9fd56cdcebddd312d7
Kevin Kuehler <keur@ocf.berkeley.edu>
2018-06-08 15:01:31 -0700

    No history. Inspired by ckuehl.

--- get.sh
+++ get.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -eu
 TARGET=https://github.com/kkuehler/dotfiles.git
-DEST=~/dotfiles
+DEST="$HOME/dotfiles"
 git clone "$TARGET" "$DEST"
 cd "$DEST"
 ./install
-source ~/.bashrc
+source "$HOME/.bashrc"
 read -rp "Are you at the OCF? [y/N] " ocf
 ocf=${ocf,,}
 if [[ "$ocf" =~ ^(yes|y) ]]; then
-    mv ~/.bashrc ~/.bashrc.local
+    echo "Moving ~/.bashrc to ~/.bashrc.local"
+    mv "$HOME/.bashrc" "$HOME/.bashrc.local"
 fi
 cd ..
+
+# clear history
+histfiles=( "lesshst"
+            "histfile"
+            "mysql_history"
+            "nano_history"
+            "sqlite_history"
+            "bash_history"
+            "python_history"
+            )
+
+for i in "${histfiles[@]}"; do
+    echo "$i -> /dev/null"
+    ln -fs /dev/null "$HOME/.$i"
+done
